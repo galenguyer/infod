3c50bd4a0488c4b8251dfb18671a15f857f3dc31
Galen Guyer <galen@galenguyer.com>
2021-06-14 17:34:38 -0700

    format things and add perf counter to api

--- api/infod/routes.py
+++ api/infod/routes.py
@@ -1,6 +1,10 @@
 from infod import app
 from flask import jsonify
+from time import perf_counter
+
 
 @app.route("/api/v1/latest")
 def _get_api_v1_latest():
-    return jsonify({'status': 'ok'}), 200
+    t_start = perf_counter()
+    t_end = perf_counter()
+    return jsonify({"status": "ok", "elapsed": round((t_end - t_start) * 1000, 2)}), 200
